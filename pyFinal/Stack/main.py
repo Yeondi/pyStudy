6acff2c9132cfcdfaf2dcaf32954ad63fedc55e0
Yeondi <atlantis2568@gmail.com>
2021-11-29 12:25:25 +0900

    cc

--- pyFinal/Stack/main.py
+++ pyFinal/Stack/main.py
@@ -2,16 +2,30 @@ from myStack import *
 
 def main():
     stack = STACK()
-
+    
+    inputData = [0,0] 
     while True:
-        # 입력 예제
-        # push 10   -> 10이 데이터에 들어감
-        # pop -> pop이 실행됨
-        # size -> size를 알려줌
-        # empty -> Yes / No로 알려줌
-        # clear -> clear한 후 print로 확인시켜줌
-        # show -> 모든 정보를 보여줌
-        # end 입력시 프로그램 종료
-        pass
+        inputData =input().split() # inputData에는 push,pop이런애들 nValue에는 숫자값들
 
-main
+        if inputData[0] == "push":
+            stack.push(inputData[1])
+            stack.show()
+        elif inputData[0] == "pop":
+            print(stack.pop())
+        elif inputData[0] == "size":
+            print(stack.size())
+        elif inputData[0] == "empty":
+            print(stack.empty())
+        elif inputData[0] == "clear":
+            stack.clear()
+            print(stack.show())
+        elif inputData[0] == "show":
+            stack.show()
+        elif inputData[0] == "top":
+            print(stack.top())
+        elif inputData[0] == "end":
+            break
+        else:
+            print("명령어를 잘못 입력하셨습니다.")
+
+main()
--- pyFinal/Stack/myStack.py
+++ pyFinal/Stack/myStack.py
@@ -1,25 +1,42 @@
 
 class STACK():
     def __init__(self) -> None:
-        pass
+        self._saveData = []
 
-    def push(self): # 맨 뒤에 넣음
-        pass
+    def setSaveData(self,data):
+        self._saveData.append(data)
 
+    def getSaveData(self):
+        return self._saveData
+
+    def push(self,data): # 맨 뒤에 넣음
+        self.setSaveData(data)
+
+    def remove(self,data):
+        self._saveData.remove(data)
+        
     def pop(self): # 맨 끝 값을 빼서 변수에 넣어주는애
-        pass
+        value = self.top()
+        self._saveData.pop()
+        return value
 
     def size(self): # 스택의 사이즈
-        pass
+        return len(self._saveData)
 
     def empty(self): # 비어있는지 / 안비어있는지 알려주는애
-        pass
+        if self.size() >= 1:
+            return False
+        else:
+            return True
 
     def clear(self): # 전부 삭제하는애
-        pass
+        self._saveData.clear()
 
     def top(self): # 맨 위에 있는 애 보여주는애
-        pass
+        dataSize = self.size()
+        return self._saveData[dataSize-1]
 
     def show(self): # 저장된 값을 보여주는애
-        pass
+        for i in self._saveData:
+            print(i," ",end='')
+        print()
